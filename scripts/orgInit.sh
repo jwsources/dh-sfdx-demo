dd8e502866a0c1ed9aa898edc62d23e64f8d4db6
salesforceplatform <jweijters@salesforce.com>
2018-10-26 10:36:17 +0200

    Package demo

--- scripts/orgInit.sh
+++ scripts/orgInit.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-DURATION=7
+DURATION=1
 
 if [ "$#" -eq 1 ]; then
   DURATION=$1
 fi
 
-sfdx force:org:create -a dreamhouse -s -f config/project-scratch-def.json -d $DURATION
-sfdx force:source:push
-sfdx force:user:permset:assign -n dreamhouse
+sfdx force:org:delete -p -u dhpack
+sfdx shane:org:create -u jwadmin -o whizzhouse.demo -a dhpack -d $DURATION -s -f config/project-scratch-def.json
+#sfdx force:source:push
+#sfdx force:user:permset:assign -n dreamhouse
+sfdx force:org:list | grep dhpack
 sfdx force:org:open -p /lightning/page/home
 echo "Org is set up"
--- scripts/packagingDeployment.sh
+++ scripts/packagingDeployment.sh
@@ -3,7 +3,8 @@
 # Default values
 BRANCH=$1
 SFDX_CLI_EXEC=sfdx
-TARGET_ORG='packaging@trailheadapps.org'
+TARGET_ORG='dhpack'
+PACKAGE_NAME='packagedemo01'
 RESULT=0
 
 # Defining Salesforce CLI exec, depending if it's CI or local dev machine
@@ -13,7 +14,8 @@ if [ $CI ]; then
   TARGET_ORG="packagingorg"
 fi
 
-PACKAGE_VERSION="$($SFDX_CLI_EXEC force:package:version:create -p dreamhouse -x -w 10 --json)"
+echo "Creating new Package version"
+PACKAGE_VERSION="$($SFDX_CLI_EXEC force:package:version:create -p $PACKAGE_NAME -x -d force-app -w 20 --json)"
 RESULT="$(echo $PACKAGE_VERSION | jq '.status')"
 echo "Result is $RESULT"
 
@@ -25,10 +27,12 @@ if [ $RESULT -gt 0 ]; then
   echo $PACKAGE_VERSION
   exit 1
 else
-  sleep 300
+  sleep 240
 fi
 
 PACKAGE_VERSION="$(echo $PACKAGE_VERSION | jq '.result.SubscriberPackageVersionId' | tr -d '"')"
 
+echo "Installing new Package version"
 $SFDX_CLI_EXEC force:package:install --package $PACKAGE_VERSION -w 10 -u $TARGET_ORG -r
 echo "Done"
+sfdx force:org:open -p /lightning/page/home
